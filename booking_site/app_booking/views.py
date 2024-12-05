9c6d1ce76079480ce6d8fc31458738c9d9e50464
axkeil1 <asemaibraimbekova16@gmail.com>
2024-12-05 16:10:13 +0600

    struktura

---
+++ booking_site/app_booking/views.py
@@ -0,0 +1,33 @@
+from .serializers import *
+from .models import *
+from rest_framework import viewsets, generics
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class HotelListApiView(generics.ListAPIView):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelListSerializer
+
+
+class HotelDetailApiView(generics.RetrieveAPIView):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelDetailSerializer
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
